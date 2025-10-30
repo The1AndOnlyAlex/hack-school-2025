7655b148c8ab092dd5f5747e0370da999df9bf13
Alex Duan <40921264+The1AndOnlyAlex@users.noreply.github.com>
2025-10-29 20:02:05 -0700

    Workshop 3, GET function works. POST function returns 404

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const pollController = require('../controllers/pollController');
 const router = express.Router();
 
-// ACTIVITY: Create routes that call controller functions when rrequested
+// ACTIVITY: Create routes that call controller functions when requested
+router.get('/polls/id/:id',pollController.getPoll)
+router.get('/polls',pollController.getPolls)
+router.get('/polls',pollController.postPoll)
+router.get('/vote',pollController.postVote)
+
 
 module.exports = router;
